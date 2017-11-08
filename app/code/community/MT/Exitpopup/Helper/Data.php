cc3b3f4b89870c8b0e72c26e945fe518bb524de8
Edvinas Stulpinas <info@magetrend.com>
2017-11-08 16:47:53 +0200

    PHPcs changes

--- app/code/community/MT/Exitpopup/Helper/Data.php
+++ app/code/community/MT/Exitpopup/Helper/Data.php
@@ -2,42 +2,63 @@
 
 class MT_Exitpopup_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
     public function isMobile()
     {
-        $is_mobile = '0';
+        $isMobile = '0';
+        $request = Mage::app()->getRequest();
+        $userAgent = $request->getServer('HTTP_USER_AGENT', false);
+        $httpAccept = $request->getServer('HTTP_ACCEPT', false);
+        $httpXWapProfile = $request->getServer('HTTP_X_WAP_PROFILE', false);
+        $httpProfile = $request->getServer('HTTP_PROFILE', false);
+        $allHttp = $request->getServer('ALL_HTTP', false);
 
-        if(preg_match('/(android|up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone)/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
-            $is_mobile=1;
+        if ($userAgent
+            && preg_match(
+                '/(android|up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone)/i',
+                strtolower($userAgent)
+            )
+        ) {
+            $isMobile = 1;
         }
 
-        if (isset($_SERVER['HTTP_ACCEPT'])) {
-            if((strpos(strtolower($_SERVER['HTTP_ACCEPT']),'application/vnd.wap.xhtml+xml')>0) or ((isset($_SERVER['HTTP_X_WAP_PROFILE']) or isset($_SERVER['HTTP_PROFILE'])))) {
-                $is_mobile=1;
+        if ($httpAccept) {
+            if ((strpos(strtolower($httpAccept), 'application/vnd.wap.xhtml+xml') !== false)
+                || (($httpXWapProfile || $httpProfile))
+            ) {
+                $isMobile = 1;
             }
         }
 
-        $mobile_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'],0,4));
-        $mobile_agents = array('w3c ','acs-','alav','alca','amoi','andr','audi','avan','benq','bird','blac','blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno','ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-','maui','maxo','midp','mits','mmef','mobi','mot-','moto','mwbp','nec-','newt','noki','oper','palm','pana','pant','phil','play','port','prox','qwap','sage','sams','sany','sch-','sec-','send','seri','sgh-','shar','sie-','siem','smal','smar','sony','sph-','symb','t-mo','teli','tim-','tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp','wapr','webc','winw','winw','xda','xda-');
+        if ($userAgent) {
+            $mobileUa = strtolower(substr($userAgent, 0, 4));
+            $mobileAgents = array(
+                'w3c ','acs-','alav','alca','amoi','andr','audi','avan','benq','bird','blac','blaz','brew',
+                'cell','cldc','cmd-','dang','doco','eric','hipt','inno','ipaq','java','jigs','kddi','keji',
+                'leno','lg-c','lg-d','lg-g','lge-','maui','maxo','midp','mits','mmef','mobi','mot-','moto',
+                'mwbp','nec-','newt','noki','oper','palm','pana','pant','phil','play','port','prox','qwap',
+                'sage','sams','sany','sch-','sec-','send','seri','sgh-','shar','sie-','siem','smal','smar',
+                'sony','sph-','symb','t-mo','teli','tim-','tosh','tsm-','upg1','upsi','vk-v','voda','wap-',
+                'wapa','wapi','wapp','wapr','webc','winw','winw','xda','xda-'
+            );
 
-        if(in_array($mobile_ua,$mobile_agents)) {
-            $is_mobile=1;
-        }
-
-        if (isset($_SERVER['ALL_HTTP'])) {
-            if (strpos(strtolower($_SERVER['ALL_HTTP']),'OperaMini')>0) {
-                $is_mobile=1;
+            if (in_array($mobileUa, $mobileAgents)) {
+                $isMobile = 1;
             }
         }
 
-        if (isset($_SERVER['HTTP_USER_AGENT'])) {
-            if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'windows')>0) {
-                $is_mobile=0;
+        if ($allHttp) {
+            if (strpos(strtolower($allHttp), 'OperaMini') !== false) {
+                $isMobile = 1;
             }
         }
 
+        if ($userAgent) {
+            if (strpos(strtolower($userAgent), 'windows') !== false) {
+                $isMobile = 0;
+            }
+        }
 
-        return $is_mobile;
+        return $isMobile;
     }
 
     public function translate($key)
